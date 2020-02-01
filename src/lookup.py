65abe7996ff6b4f332f01dbe4624df6835932aaa
ryuichi1208 <ryucrosskey@gmail.com>
2020-02-01 22:14:30 +0900

    first commit

---
+++ src/lookup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import socket
+import sys
+
+class HogeCaller:
+    def call_hoge(self):
+        return 'called: ' + Hoge().hoge_method()
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
---
+++ tests/test_lookup.py
@@ -0,0 +1,1 @@
+from src.lookup import HogeCaller
